74ce34e0ac59902c9dffb7977bfc2b23fa9f54e2
suraj-khot-19 <surajkhot19082001@gmail.com>
2023-08-06 12:12:12 +0530

    filter in stream

---
+++ CodeWithSuraj/CWS_64_Stream_Filter.java
@@ -0,0 +1,16 @@
+package CodeWithSuraj;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CWS_64_Stream_Filter {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Stream<Integer> even=list.stream().filter(n->n%2==0);
+
+        List<Integer> evenList = even.toList();
+        System.out.println(evenList);
+    }
+}
