9ea11736a7ff02419ef16edc9da48cda8d22e679
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-07-20 12:20:00 +0530

    factorial of number

---
+++ CodeWithSuraj/CWS_71_factorial.java
@@ -0,0 +1,14 @@
+package CodeWithSuraj;
+
+public class CWS_71_factorial {
+    public static void main(String[] args) {
+        int n=5;
+        int fact=1;
+
+        for (int i=1;i<=n;i++){
+            fact*=i;
+        }
+
+        System.out.println(fact);
+    }
+}
