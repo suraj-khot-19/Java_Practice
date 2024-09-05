0a6ca1f0d79285de259f4c3c5da8db54ce3a6265
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-09-05 12:20:00 +0530

    factorial of number using recurssion

---
+++ CodeWithSuraj/COWS_72_Factorial_Recursions.java
@@ -0,0 +1,15 @@
+package CodeWithSuraj;
+
+public class COWS_72_Factorial_Recursions {
+    static int fact(int n){
+        if(n!=0){
+            return n*fact(n-1);
+        }else{
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(fact(n));
+    }
+}
