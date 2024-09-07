08ab0cecf5be3c4b8027a5c96280f1e8482713e4
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-09-07 12:20:00 +0530

    gcd of number

---
+++ CodeWithSuraj/CWS_73_GCD.java
@@ -0,0 +1,16 @@
+package CodeWithSuraj;
+
+public class CWS_73_GCD {
+    public static void main(String[] args) {
+        int n1=12;
+        int n2=16;
+        int gcd=1;
+
+        for (int i=1;i<n1&&i<n2;i++){
+            if(n1%i==0 && n2%i==0){
+                gcd=i;
+            }
+        }
+        System.out.println(gcd);
+    }
+}
