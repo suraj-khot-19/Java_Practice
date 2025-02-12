30ffe66123f6d7259d04fec69fbdaa3df2e121ad
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-12 21:54:26 +0530

    left triangle pattern

---
+++ Logic_Building/CWS_09_left_side_pattern.java
@@ -0,0 +1,24 @@
+/*
+
+        *
+      * *
+    * * *
+   * * * *
+ * * * * *
+
+ */
+
+
+public class CWS_09_left_side_pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int z = 2 * (5 - i); z >= 0; z--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
