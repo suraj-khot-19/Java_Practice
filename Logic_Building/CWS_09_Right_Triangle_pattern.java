643d0fbafcc6d165e2b7f326c346174f8961f21c
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-12 21:37:51 +0530

    right triangle pattern

---
+++ Logic_Building/CWS_09_Right_Triangle_pattern.java
@@ -0,0 +1,19 @@
+/*
+
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+
+ */
+public class CWS_09_Right_Triangle_pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j >= 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
